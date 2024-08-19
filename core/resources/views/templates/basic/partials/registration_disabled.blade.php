e61add320dd165dfa11e526949ba4fa262f5e438
Mahe Karim <mahekarim@gmail.com>
2024-08-19 18:33:40 +0600

    Register Disabled Page

--- core/resources/views/templates/basic/partials/registration_disabled.blade.php
+++ core/resources/views/templates/basic/partials/registration_disabled.blade.php
@@ -2,19 +2,42 @@
     $registrationDisabled = getContent('register_disable.content',true);
 @endphp
 <section class="account section-bg maintenance-page flex-column justify-content-center">
-<div class="register-disable">
-    <div class="container text-center">
-        <div class="register-disable-image">
-            <img class="fit-image" src="{{ frontendImage('register_disable',@$registrationDisabled->data_values->image,'280x280') }}" alt="">
-        </div>
-
-        <h5 class="register-disable-title mt-4">{{ __(@$registrationDisabled->data_values->heading) }}</h5>
-        <p class="register-disable-desc">
-            {{ __(@$registrationDisabled->data_values->subheading) }}
-        </p>
-        <div class="text-center mt-3">
-            <a href="{{ @$registrationDisabled->data_values->button_url }}" class="register-disable-footer-link btn btn-sm btn--base">{{ __(@$registrationDisabled->data_values->button_name) }}</a>
+    <div class="register-disable">
+        <div class="container text-center">
+            <div class="register-disable-image">
+                <img class="fit-image"
+                     src="{{ frontendImage('register_disable',@$registrationDisabled->data_values->image,'280x280') }}"
+                     alt="">
+            </div>
+            <h5 class="register-disable-title mt-4">{{ __(@$registrationDisabled->data_values->heading) }}</h5>
+            <p class="register-disable-desc">
+                {{ __(@$registrationDisabled->data_values->subheading) }}
+            </p>
+            <div class="text-center mt-3">
+                <a href="{{ @$registrationDisabled->data_values->button_url }}"
+                   class="register-disable-footer-link btn btn-sm btn--base">{{ __(@$registrationDisabled->data_values->button_name) }}</a>
+            </div>
         </div>
     </div>
-</div>
 </section>
+
+<style>
+    .header {
+        display: none;
+    }
+    body, .register-disable {
+        background-color: white !important;
+        display: flex;
+        align-items: center;
+        height: 100vh;
+        justify-content: center;
+    }
+
+    .breadcrumb {
+        display: none;
+    }
+
+    .footer-area {
+        display: none;
+    }
+</style>
