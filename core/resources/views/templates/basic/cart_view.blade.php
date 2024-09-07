75561576b754faf34787d9095587ebea74a4389b
Mahe Karim <mahekarim@gmail.com>
2024-09-07 18:31:54 +0600

    Problem Solved

--- core/resources/views/templates/basic/cart_view.blade.php
+++ core/resources/views/templates/basic/cart_view.blade.php
@@ -3,73 +3,69 @@
     <div class="cart-section py-60 bg-white">
         <div class="container">
             <div class="cart-header">
-                <div class="status-check d-flex align-items-center justify-content-between gap-3 mb-4">
-                    <div class="form-check form--check all">
-                        <input class="form-check-input checkAll" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                            @lang('Select All')
-                        </label>
-                    </div>
-                    <button type="button" class="btn btn--base btn--sm clear-all">@lang('Clear All')</button>
-                </div>
                 <div class="cart">
                     <div class="cart-body">
-                        @foreach ($carts as $cart)
-                            @php
-                                $user     = auth()->user() ?? null;
-                                $price    = showDiscountPrice($cart->product->price, $cart->product->discount, $cart->product->discount_type);
-                            @endphp
-                            <div class="cart-item">
-                                <div class="cart-item__content">
-                                    <div class="form-check form--check">
-                                        <input class="form-check-input input-check" type="checkbox" value="">
-                                    </div>
-                                    <div class="thumb">
-                                        <img src="{{ getImage(getFilePath('product') . '/' . $cart->product->image, getFileSize('product')) }}"
-                                             alt="@lang($cart->product->name)">
-                                    </div>
-                                    <div class="inner-content">
-                                        <small class="inner-content__name">
-                                            @lang('Brand:') {{ __($cart->product->brand->name) }}
-                                        </small>
-                                        <a href="{{ route('product.details',  $cart->product_id) }}" class="productName"
-                                           data-product_id="{{ $cart->product_id }}">
-                                            {{ __($cart->product->name) }}
-                                        </a>
-                                        <div class="size">
-                                            <div>
-                                                <span class="size__text">@lang('Price:')</span>
-                                                <span class="price">{{ showAmount($price) }}</span>
+                        @if($carts->isEmpty())
+                            <div class="text-center py-5">
+                                <h5>@lang('No Cart found')</h5>
+                            </div>
+                        @else
+                            @foreach ($carts as $cart)
+                                @php
+                                    $user     = auth()->user() ?? null;
+                                    $price    = showDiscountPrice($cart->product->price, $cart->product->discount, $cart->product->discount_type);
+                                @endphp
+                                <div class="cart-item">
+                                    <div class="cart-item__content">
+                                        <div class="thumb">
+                                            <img src="{{ getImage(getFilePath('product') . '/' . $cart->product->image, getFileSize('product')) }}"
+                                                 alt="@lang($cart->product->name)">
+                                        </div>
+                                        <div class="inner-content">
+                                            <small class="inner-content__name">
+                                                @lang('Brand:') {{ __($cart->product->brand->name) }}
+                                            </small>
+                                            <a href="{{ route('product.details',  $cart->product_id) }}" class="productName"
+                                               data-product_id="{{ $cart->product_id }}">
+                                                {{ __($cart->product->name) }}
+                                            </a>
+                                            <div class="size">
+                                                <div>
+                                                    <span class="size__text">@lang('Price:')</span>
+                                                    <span class="price">{{ showAmount($price) }}</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="qty-container">
+                                        <button class="qty-btn-minus btn-light" type="button"><i class="fa fa-minus"></i></button>
+                                        <input type="number" class="form-control input-qty" name="quantity" value="{{ $cart->quantity }}">
+                                        <button class="qty-btn-plus btn-light" type="button"><i class="fa fa-plus"></i></button>
+                                    </div>
+                                    <div class="d-flex flex-column text-center">
+                                        <span class="amount">@lang('Price')</span>
+                                        <span class="fs-16">
+                                            <span class="subtotal">{{ showAmount($price * $cart->quantity) }}</span>
+                                        </span>
+                                    </div>
+                                    <button class="btn--danger remove-btn"><i class="las la-trash"></i></button>
                                 </div>
-                                <div class="qty-container">
-                                    <button class="qty-btn-minus btn-light" type="button"><i class="fa fa-minus"></i></button>
-                                    <input type="number" class="form-control input-qty" name="quantity" value="{{ $cart->quantity }}">
-                                    <button class="qty-btn-plus btn-light" type="button"><i class="fa fa-plus"></i></button>
-                                </div>
-                                <div class="d-flex flex-column text-center">
-                                    <span class="amount">@lang('Subtotal Price')</span>
-                                    <span class="fs-16">
-                                        <span class="subtotal">{{ showAmount($price * $cart->quantity) }}</span>
-                                    </span>
-                                </div>
-                                <button class="btn--danger remove-btn"><i class="las la-trash"></i></button>
-                            </div>
-                        @endforeach
+                            @endforeach
 
-                        <div class="d-flex justify-content-end coupon-content">
-                            <div class="text-end fs-16">
-                                <small class="price-title d-block">@lang('Total Price') </small>
-                                <span class="cartSubtotal value total-price">{{ gs('cur_sym') }}0.00</span>
+                            <div class="d-flex justify-content-end coupon-content">
+                                <div class="text-end fs-16 price-content">
+                                    <small class="price-title d-block">@lang('Total Price') </small>
+                                    <span class="cartSubtotal value total-price">{{ gs('cur_sym') }}0.00</span>
+                                </div>
                             </div>
-                        </div>
+                        @endif
                     </div>
                 </div>
-                <div class="checkout-btn text-end">
-                    <a href="{{ route('user.checkout.index') }}" class="btn btn--base checkoutBtn"> @lang('Checkout') </a>
-                </div>
+                @if(!$carts->isEmpty())
+                    <div class="checkout-btn text-end">
+                        <a href="{{ route('user.checkout.index') }}" class="btn btn--base checkoutBtn"> @lang('Checkout') </a>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
@@ -103,26 +99,6 @@
             let removeableItems = [];
             let modal = $('#removeCartModal');
 
-            // Select or deselect all checkboxes
-            $('.checkAll').on('change', function () {
-                $('.input-check').prop('checked', $(this).is(':checked'));
-            });
-
-            // Handle 'Clear All' button click
-            $('.clear-all').on('click', function () {
-                removeableItems = [];
-                $('.input-check:checked').each(function () {
-                    let cartItem = $(this).closest('.cart-item');
-                    removeableItems.push(cartItem);
-                });
-
-                if (removeableItems.length === 0) {
-                    notify('error', 'No items selected to remove.');
-                    return;
-                }
-                modal.modal('show');
-            });
-
             // Handle individual item removal
             $('.remove-btn').on('click', function () {
                 removeableItems = [$(this).closest('.cart-item')];
@@ -154,6 +130,12 @@
                                 getCartCount();
                                 subTotal();
                                 notify('success', response.success);
+
+                                if ($('.cart-item').length === 0) {
+                                    $('.price-content').addClass('d-none');
+                                    $('.checkoutBtn').addClass('d-none');
+                                    $('.cart-body').html('<div class="text-center py-5"><h5>@lang("No Cart found")</h5></div>');
+                                }
                             } else {
                                 notify('error', response.error);
                             }
@@ -189,16 +171,21 @@
                         }
                     }
                 });
+
                 $('.total-price').text("{{ gs('cur_sym') }}" + subtotal.toFixed(2));
 
-                // Show/hide checkout button based on subtotal
+                // Show/hide checkout button and price section based on subtotal
                 if (subtotal > 0) {
                     $('.checkoutBtn').removeClass('d-none');
+                    $('.price-content').removeClass('d-none');
                 } else {
                     $('.checkoutBtn').addClass('d-none');
+                    $('.price-content').addClass('d-none');
+                    if ($('.cart-item').length === 0) {
+                        $('.cart-body').html('<div class="text-center py-5"><h5>@lang("No Cart found!")</h5></div>');
+                    }
                 }
             }
-
 
             function initializeQuantityButtons() {
                 $('.qty-btn-plus, .qty-btn-minus').on('click', function() {
--- core/resources/views/templates/basic/user/payment/manual.blade.php
+++ core/resources/views/templates/basic/user/payment/manual.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row justify-content-left">
+            <div class="col-md-10">
                 <div class="card custom--card">
                     <div class="card-header card-header-bg">
                         <h5 class="card-title">{{__($pageTitle)}}</h5>
